0a1fb59e941852e39244639d180b11348a879baa
Tareq Judeh <tareq.joudeh@gmail.com>
2020-11-16 14:27:44 +0000

    Uploading project files to GitHub.

--- main.py
+++ main.py
@@ -1,2 +1,47 @@
 from replit import clear
-#HINT: You can call clear() to clear the output in the console.
+from  art import logo
+
+print(logo)
+print('''
+''')
+
+participants = dict()
+auction = True
+
+while auction:
+  bidder = input("Enter your name? ")
+  bid = int(input("Enter your bid amount: $"))
+  participants[bidder] = bid
+  print("")
+  check_for_bidders = True
+  while check_for_bidders:
+
+    more_bidders = input("Are there any more bidder? (y/n) ").lower()
+    if more_bidders == "y": 
+      clear()
+      check_for_bidders = False
+
+    elif more_bidders == "n": 
+      auction = False
+      check_for_bidders = False
+    else:
+      print("Bad input! Please type 'y' or 'n' for your answer.")
+
+# Auction participants:
+print("")
+print("Auction participants:")
+for k, v in participants.items():
+  print(f"  {k}, Bid amount: ${v}")
+
+highest_bid = 0
+winner = ""
+
+for k, v in participants.items():
+  if v > highest_bid:
+    highest_bid = v
+    winner = k
+print("")
+print(f"{winner} is the highest bidder with amount ${highest_bid} and is the auction winner.")
+
+
+
